5c595678a870391c37a41215f9f1ed006972b1e9
Unknown <rachanakesarkar03@gmail.com>
2018-12-05 16:07:16 +0530

    Assignment-7
    Finding width,height,x and y values

---
+++ Box/box1.js
@@ -0,0 +1,10 @@
+var h=1024,w=600;
+var x1=35,y1=60;
+var x3=function(){
+    return(x1+w-1)
+}
+console.log("x3="+x3());
+var y3=function(){
+    return(y1+h-1)
+}
+console.log("y3="+y3())
---
+++ Box/box2.js
@@ -0,0 +1,10 @@
+var h=1024,w=600;
+var x1,y1;
+var x3=function(){
+    return((w*10)/100)
+}
+console.log("x3="+x3());
+var y3=function(){
+    return((h*10)/100)
+}
+console.log("y3="+y3())
