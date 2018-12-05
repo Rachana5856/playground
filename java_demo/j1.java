a4bdf5e57f8186aa229140158f89714c7a2d299c
Unknown <rachanakesarkar03@gmail.com>
2018-12-05 15:55:46 +0530

    Assignment-4
    Basic java programs

---
+++ java_demo/j1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+//import java.lang.*;
+ public class j1{
+        public static void main(String args[]){
+        String s;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter a string");
+        s=obj.nextLine();
+        System.out.println(s.trim());
+    }
+}
